2efca162266aca0c8cfeb97ea020febf56de4b02
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2025-06-22 13:29:41 +0200

    feat: add performance test on goedel

--- tests/performance/test_benchmark.py
+++ tests/performance/test_benchmark.py
@@ -4,21 +4,26 @@ from typing import List
 import pytest
 from datasets import load_dataset
 from fastapi.testclient import TestClient
+from loguru import logger
 
 from fast_repl.main import app
 
 
-@pytest.mark.performance
+@pytest.mark.performance  # type: ignore[misc]
 def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> None:
-    ds = load_dataset("Goedel-LM/Lean-workbook-proofs", split="train")
+    ds = load_dataset(
+        "Goedel-LM/Lean-workbook-proofs", split="train"
+    )  # TODO: check which lean version is this?
     if perf_shuffle:
         ds = ds.shuffle(seed=0)
     if perf_rows:
         ds = ds.select(range(perf_rows))
 
+    logger.info(f"Number of proofs to benchmark: {len(ds)}")
     times: List[float] = []
     with TestClient(app) as client:
         for item in ds:
+            logger.info(f"Running proof: {item["problem_id"]}")
             proof = item["full_proof"]
             resp = client.post("/repl", json={"cmd": proof})
             assert resp.status_code == 200
@@ -26,5 +31,10 @@ def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> None:
             assert "time" in data
             times.append(float(data["time"]))
 
-    print("Benchmark results for", len(times), "proofs (shuffle=" + str(perf_shuffle) + ")")
-    print("min:", min(times), "max:", max(times), "mean:", mean(times))
+    logger.info(
+        f"Benchmark results for {len(times)} proofs (shuffle= {str(perf_shuffle)} ",
+    )
+    logger.info(
+        f"min: {min(times):.2f} s, max: {max(times):.2f} s and mean: {mean(times):.2f} s"
+    )
+    assert mean(times) < 30, "Mean time for proofs should be less than 10 seconds"
