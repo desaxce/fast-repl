097cf7cff1ecee79d2b1f800a60c77c06cf42ea6
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2025-06-22 10:32:37 -0400

    [fast-repl] run benchmark tests concurrently

--- tests/performance/test_benchmark.py
+++ tests/performance/test_benchmark.py
@@ -1,16 +1,20 @@
+from __future__ import annotations
+
+import asyncio
 from statistics import mean
-from typing import List
 
 import pytest
 from datasets import load_dataset
-from fastapi.testclient import TestClient
+import httpx
 from loguru import logger
 
 from fast_repl.main import app
+from fast_repl import settings
 
 
 @pytest.mark.performance  # type: ignore[misc]
-def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> None:
+@pytest.mark.asyncio
+async def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> None:
     ds = load_dataset(
         "Goedel-LM/Lean-workbook-proofs", split="train"
     )  # TODO: check which lean version is this?
@@ -20,16 +24,23 @@ def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> None:
         ds = ds.select(range(perf_rows))
 
     logger.info(f"Number of proofs to benchmark: {len(ds)}")
-    times: List[float] = []
-    with TestClient(app) as client:
-        for item in ds:
-            logger.info(f"Running proof: {item["problem_id"]}")
-            proof = item["full_proof"]
-            resp = client.post("/repl", json={"cmd": proof})
-            assert resp.status_code == 200
-            data = resp.json()
-            assert "time" in data
-            times.append(float(data["time"]))
+    times: list[float] = []
+
+    async with httpx.AsyncClient(app=app, base_url="http://test") as client:
+        semaphore = asyncio.Semaphore(settings.MAX_REPLS)
+
+        async def run_item(item: dict[str, str]) -> None:
+            async with semaphore:
+                logger.info(f"Running proof: {item['problem_id']}")
+                proof = item["full_proof"]
+                resp = await client.post("/repl", json={"cmd": proof})
+                assert resp.status_code == 200
+                data = resp.json()
+                assert "time" in data
+                times.append(float(data["time"]))
+
+        tasks = [asyncio.create_task(run_item(item)) for item in ds]
+        await asyncio.gather(*tasks)
 
     logger.info(
         f"Benchmark results for {len(times)} proofs (shuffle= {str(perf_shuffle)} ",
