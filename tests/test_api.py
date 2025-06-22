8e62759e5ddf7a361fe6af9a3d7c383074e8772f
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2025-06-22 09:13:18 +0200

    fix: add test mathlib

--- tests/test_api.py
+++ tests/test_api.py
@@ -24,3 +24,21 @@ def test_repl_check_nat() -> None:
         }
         for key, value in expected.items():
             assert resp.json()[key] == value
+
+
+def test_repl_mathlib() -> None:
+    with TestClient(app) as client:
+        resp = client.post("/repl", json={"cmd": "import Mathlib"})
+        assert resp.status_code == 200
+
+        resp1 = client.post(
+            "/repl", json={"cmd": "import Mathlib\ndef f := 2", "env": 0}
+        )
+        assert resp1.status_code == 200
+
+    expected = {"env": 0}
+    for key, value in expected.items():
+        assert resp.json()[key] == value
+
+    assert resp.json()["time"] < 15
+    assert resp1.json()["time"] < 1
