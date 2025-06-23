0429fa295f996003ddf79fdf2b10d4e6295d268a
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2025-06-23 11:54:36 +0200

    feat: pool

--- src/fast_repl/main.py
+++ src/fast_repl/main.py
@@ -1,25 +1,22 @@
-from typing import Any, AsyncGenerator
+from typing import Any
 
 from fastapi import FastAPI, HTTPException
-from fastapi.concurrency import asynccontextmanager
 from loguru import logger
 
-from fast_repl.errors import PoolError
-from fast_repl.repl import Command
+from fast_repl.errors import PoolError, ReplError
+from fast_repl.repl import Command, Repl
 from fast_repl.repl_pool import ReplPoolManager
 
+# @asynccontextmanager  # type: ignore
+# async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+#     await app.state.pool.init_pool()
+#     logger.info("Initialized pool!")
+#     yield
+#     await app.state.pool.cleanup()
+#     logger.info("Cleaned up pool!")
 
-@asynccontextmanager  # type: ignore
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    await app.state.pool.init_pool()
-    logger.info("Initialized pool!")
-    yield
-    await app.state.pool.cleanup()
-    logger.info("Cleaned up pool!")
-
-
-app = FastAPI()
-app.state.pool = ReplPoolManager()
+app = FastAPI(logger=logger)
+pool: ReplPoolManager = ReplPoolManager()
 
 
 # TODO: make it a health endpoint + add stats
@@ -28,10 +25,16 @@ def read_root() -> dict[str, str]:
     return {"message": "Hello, World!"}
 
 
-@app.post("/repl/")  # type: ignore
+@app.post("/repl")  # type: ignore
 async def send_repl(command: Command) -> Any:  # TODO: fix Any typing
     try:
-        repl = await app.state.pool.get_repl()
+        repl: Repl = await pool.get_repl()
+        if not repl.is_started:
+            try:
+                await repl.start()  # here add header
+            except ReplError:
+                await pool.release_repl(repl)
+                raise HTTPException(500, "Error occurred: failed to start REPL")
     except PoolError:
         raise HTTPException(429, "REPL pool exhausted")
 
@@ -40,5 +43,4 @@ async def send_repl(command: Command) -> Any:  # TODO: fix Any typing
     except Exception as e:
         raise HTTPException(500, str(e)) from e
     finally:
-        await app.state.pool.release_repl(repl)
-        logger.info("DONE releasing")
+        await pool.release_repl(repl)
--- tests/performance/test_benchmark.py
+++ tests/performance/test_benchmark.py
@@ -1,19 +1,25 @@
 from __future__ import annotations
 
 import asyncio
+import os
 from statistics import mean
 
+import httpx
 import pytest
 from datasets import load_dataset
-import httpx
+from fastapi.testclient import TestClient
+from httpx import ASGITransport, AsyncClient
 from loguru import logger
+from tqdm import tqdm
 
 from fast_repl.main import app
-from fast_repl import settings
+from fast_repl.settings import settings
+
+# TODO: print json commands and snippets nicely + pretty cat the proofs sent (replacing \n with eols)
 
 
 @pytest.mark.performance  # type: ignore[misc]
-@pytest.mark.asyncio
+@pytest.mark.asyncio  # type: ignore[misc]
 async def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> None:
     ds = load_dataset(
         "Goedel-LM/Lean-workbook-proofs", split="train"
@@ -23,15 +29,19 @@ async def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> No
     if perf_rows:
         ds = ds.select(range(perf_rows))
 
-    logger.info(f"Number of proofs to benchmark: {len(ds)}")
+    logger.info(f"Checking {len(ds)} proofs")
     times: list[float] = []
 
-    async with httpx.AsyncClient(app=app, base_url="http://test") as client:
-        semaphore = asyncio.Semaphore(settings.MAX_REPLS)
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://testserver"
+    ) as client:
+        logger.debug(
+            f"max_repls: {settings.MAX_REPLS}, max_reuse: {settings.MAX_REUSE}"
+        )
+        semaphore = asyncio.Semaphore(settings.MAX_REPLS)  # limit concurrent requests
 
         async def run_item(item: dict[str, str]) -> None:
             async with semaphore:
-                logger.info(f"Running proof: {item['problem_id']}")
                 proof = item["full_proof"]
                 resp = await client.post("/repl", json={"cmd": proof})
                 assert resp.status_code == 200
@@ -39,13 +49,21 @@ async def test_proof_dataset_benchmark(perf_rows: int, perf_shuffle: bool) -> No
                 assert "time" in data
                 times.append(float(data["time"]))
 
-        tasks = [asyncio.create_task(run_item(item)) for item in ds]
-        await asyncio.gather(*tasks)
+        tasks = [
+            asyncio.create_task(run_item(item))
+            for item in ds
+            if item["problem_id"]
+            not in [
+                "lean_workbook_10036",
+                # "lean_workbook_1003",
+            ]  # skip this one, it's too long
+        ]
 
-    logger.info(
-        f"Benchmark results for {len(times)} proofs (shuffle= {str(perf_shuffle)} ",
-    )
+        for fut in tqdm(asyncio.as_completed(tasks), total=len(tasks), desc="Proofs"):
+            await fut
     logger.info(
         f"min: {min(times):.2f} s, max: {max(times):.2f} s and mean: {mean(times):.2f} s"
     )
-    assert mean(times) < 30, "Mean time for proofs should be less than 10 seconds"
+    assert (
+        mean(times) < 10
+    ), "Mean time for proofs should be less than 10 seconds"  # max repls = 5
